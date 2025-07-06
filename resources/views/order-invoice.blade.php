8c6c721552a38a5fea11ebc852905a7d582c0e03
Md. Anik Rahman <a7604366@gmail.com>
2025-07-06 14:03:37 +0600

    order invoice  title  fix

--- resources/views/order-invoice.blade.php
+++ resources/views/order-invoice.blade.php
@@ -7,7 +7,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title> Dealseller :: Order Invoice </title>
+        <title> Integmeds :: Order Invoice </title>
         <meta name="robots" content="noindex,nofollow" />
         <meta name="viewport" content="width=device-width; initial-scale=1.0;" />
         <style type="text/css">
