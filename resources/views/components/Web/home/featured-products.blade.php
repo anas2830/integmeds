025e48ceeeca92a6303ff9dee8c3d908681cf6fa
Md. Anik Rahman <a7604366@gmail.com>
2025-07-09 01:54:14 +0600

    home featured product & css fixes

--- resources/views/components/Web/home/featured-products.blade.php
+++ resources/views/components/Web/home/featured-products.blade.php
@@ -2,6 +2,7 @@
 <section class="special-products-area">
     <div class="row">
         @foreach($featuredProducts as $featuredProduct)
+            @if($featuredProduct->product)
             <div class="col-lg-4">
                 <div class="special-product-wrap">
                     <div class="sp-product-img">
@@ -13,13 +14,16 @@
                         <h3>{{ optional($featuredProduct->product)->product_name }}</h3>
                         <p>{{ optional($featuredProduct->product)->short_description }}</p>
                     </div>
-                    <div class="common-btn-wrap mt-3 d-flex justify-content-center">
-                        <div class="common-btn-borders">
-                            <a class="common-btn" href="{{$featuredProduct->btn_url}}">{{$featuredProduct->btn_text}}</a>
+                    @if(!empty($featuredProduct->btn_text))
+                        <div class="common-btn-wrap mt-3 d-flex justify-content-center">
+                            <div class="common-btn-borders">
+                                <a class="common-btn" href="{{$featuredProduct->btn_url}}">{{$featuredProduct->btn_text}}</a>
+                            </div>
                         </div>
-                    </div>
+                    @endif
                 </div>
             </div>
+            @endif
         @endforeach
     </div>
 </section>
