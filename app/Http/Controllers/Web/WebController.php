f40607a3fe1d1755432615e89536faa03e39f08b
Md. Anik Rahman <a7604366@gmail.com>
2025-07-21 00:59:28 +0600

    fixes

--- app/Http/Controllers/Web/WebController.php
+++ app/Http/Controllers/Web/WebController.php
@@ -79,7 +79,7 @@ class WebController extends SidebarService
 
     public function bundle()
     {
-        $productBundles = Bundle::select('id', 'name', 'icon_path')->with(['firstImage:id,bundle_id,image_url'])->where('status', 1)->orderBy('id', 'desc')->paginate(16);
+        $productBundles = Bundle::select('id', 'name', 'icon_path')->with(['firstImage:id,bundle_id,image_url'])->where('status', 1)->orderBy('id', 'desc')->paginate(12);
         return view('Web.Layout.pages.bundle', compact('productBundles'));
     }
     public function bundleDetails($id)
