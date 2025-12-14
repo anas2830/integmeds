3615b6ab04e4a6c9db7335ca06542edc68384684
Anas <anas.ahammad@selisegroup.com>
2025-12-14 10:24:18 +0600

    cupon code create issue fix

--- app/Services/CuponCrudService.php
+++ app/Services/CuponCrudService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Models\Cupon;
+use Illuminate\Validation\ValidationException;
 
 class CuponCrudService
 {
@@ -23,6 +24,11 @@ class CuponCrudService
 
     public function createCupon($request)
     {
+        if(Cupon::where('code', $request->code)->exists()){
+            throw ValidationException::withMessages([
+                'error' => "Coupon code already exists."
+            ]);
+        }
         $cupon = new Cupon();
         $cupon->code = $request->code;
         $cupon->type = $request->type;
@@ -43,6 +49,11 @@ class CuponCrudService
 
     public function updateCupon($request, $id)
     {
+        if(Cupon::where('code', $request->code)->where('id', '!=', $id)->exists()){
+            throw ValidationException::withMessages([
+                'error' => "Coupon code already exists."
+            ]);
+        }
         $cupon = Cupon::find($id);
         $cupon->code = $request->code;
         $cupon->type = $request->type;
